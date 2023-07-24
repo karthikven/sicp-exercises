007ab3499464ae14e9d2902244d3218eff9a4ea7
karthikven <kv771@nyu.edu>
2023-07-24 17:38:50 +0530

    ex 1.2 and 1.3

---
+++ chapter1/exercise1_3.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+; Exercise 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+(define (square x) (* x x))
+(define (greatest-two-sum-square a b c) (cond
+                                       ((and (<= a b) (<= a c)) (+ (square b) (square c)))
+                                       ((and (<= b a) (<= b c)) (+ (square a) (square c)))
+                                       (else (+ (square a) (square b)))
+                                       ))
+(greatest-two-sum-square 0 1 4)
